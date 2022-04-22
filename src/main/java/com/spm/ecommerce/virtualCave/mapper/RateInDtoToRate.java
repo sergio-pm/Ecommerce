465e2981aea1e242422918648cd94b7513efc76b
sergio-pm <83302423+sergio-pm@users.noreply.github.com>
2022-04-22 16:23:12 +0200

    Service and IMpapper Committ

--- src/main/java/com/spm/ecommerce/virtualCave/mapper/RateInDtoToRate.java
+++ src/main/java/com/spm/ecommerce/virtualCave/mapper/RateInDtoToRate.java
@@ -1,4 +1,25 @@
 package com.spm.ecommerce.virtualCave.mapper;
 
-public class RateInDtoToRate {
+import com.spm.ecommerce.virtualCave.persistence.entitys.Rate;
+import com.spm.ecommerce.virtualCave.service.dto.RateInDto;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+
+@Component
+public class RateInDtoToRate implements IMapper<RateInDto, Rate> {
+    @Override
+    public Rate map(RateInDto in) {
+        Rate rate =new Rate();
+        rate.setBrandId(in.getBrandId());
+
+
+      brandId;
+        productId;
+        startDate;
+        endDate;
+        price;
+        currencyCode;
+        return null;
+    }
 }
