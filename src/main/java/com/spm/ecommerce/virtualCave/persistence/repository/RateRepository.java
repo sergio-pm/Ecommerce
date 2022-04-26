161d86e06a0e37985471779994fbd5c8e5af4e9f
sergio-pm <83302423+sergio-pm@users.noreply.github.com>
2022-04-26 22:08:04 +0200

    Add files via upload

--- src/main/java/com/spm/ecommerce/virtualCave/persistence/repository/RateRepository.java
+++ src/main/java/com/spm/ecommerce/virtualCave/persistence/repository/RateRepository.java
@@ -1,12 +1,13 @@
 package com.spm.ecommerce.virtualCave.persistence.repository;
 
-import com.spm.ecommerce.virtualCave.persistence.entitys.Rate;
+import com.spm.ecommerce.virtualCave.persistence.entities.Rate;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface RateRepository extends JpaRepository<Rate, Long> {
 
@@ -15,4 +16,8 @@ public interface RateRepository extends JpaRepository<Rate, Long> {
     @Modifying
     @Query(value="Update t_rates set price=:price where id=:id", nativeQuery = true)
     public void updateRatePrice(@Param("id") Long id, @Param("price") int price);
+
+
+    @Query(value="SELECT * FROM t_rates WHERE START_DATE=:fecha AND PRODUCT_ID =:id_product AND BRAND_ID=:id_brand",nativeQuery = true)
+    public List<Rate> getRateByDate(@Param("fecha") String fecha, @Param("id_brand") int id_brand, @Param("id_product") int product);
 }
